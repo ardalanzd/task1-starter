7592282c3aeb7bf9ff1c77861f5ceff9d7c3a8b4
ardalanz <ardalanz@gmail.com>
2026-05-26 10:25:44 +0330

    Add main.java file to git.

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Git Practice!");
+        greet("Student");
+    }
+
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+}
